78dc5ac8eaa93c73d04e577f663d55dde8073bdf
Ivan <ivan@hennig.net.br>
2019-06-02 17:53:33 -0300

    Changed to use exceptions

--- src/Config.php
+++ src/Config.php
@@ -4,30 +4,46 @@ namespace Hennig\Common;
 
 class Config
 {
-    static public function getOrError($k)
+    /**
+     * Get a key from config file or throws an error when not found
+     *
+     * @param string $key
+     * @return mixed
+     * @throws \Exception
+     */
+    static public function getOrError($key)
     {
-        $v = static::get($k);
-        if (empty($v)) {
-            throw new \Exception("Configuraçao $k está vazia");
+        $value = static::get($key);
+        if (empty($value)) {
+            throw new EKeyNotFound($key);
         }
-        return $v;
+
+        return $value;
     }
 
-    static public function get($k, $def = '')
+    /**
+     * Get a key from config file or use defautl when not found
+     *
+     * @param string $key
+     * @param string $default
+     * @return mixed
+     * @throws \Exception
+     */
+    static public function get($key, $default = '')
     {
         static $return = null;
         if ($return === null) {
             if (!defined('BASE_DIR')) {
-                throw new \Exception("Please set a BASE_DIR constant.");    
+                throw new EWrongSetting('BASE_DIR');
             }
             
-            if (!file_exists(BASE_DIR . 'config.json')) {
-                return $def;
+            if (!\file_exists(BASE_DIR . 'config.json')) {
+                return $default;
             }
             
             $return = json_decode(file_get_contents(BASE_DIR . 'config.json'), true);        
         }
 
-        return $return[$k] ?? $def;
+        return $return[$key] ?: '' ?? $default;
     }
 }
