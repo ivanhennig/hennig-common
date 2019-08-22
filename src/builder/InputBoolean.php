18acef14f5b4dfcc4b95a1d404534419d6e9e4a3
Ivan <ivan@hennig.net.br>
2019-08-22 11:34:33 -0300

    Added docs and gettext

--- src/builder/InputBoolean.php
+++ src/builder/InputBoolean.php
@@ -2,17 +2,25 @@
 
 namespace Hennig\Builder;
 
-
+/**
+ * Class InputBoolean
+ * Render a button with Yes|No options
+ *
+ * @package Hennig\Builder
+ */
 class InputBoolean extends InputSelect
 {
     public $subtype = "boolean";
 
+    /**
+     * @return InputCommon
+     */
     public function init()
     {
         $this->items = [
-            "" => __("-- Select one --"),
-            "1 " => __("Yes"),
-            "0 " => __("No")
+            '' => '--' . _('Select one') . '--',
+            '1 ' => _('Yes'),
+            '0 ' => _('No')
         ];
 
         return parent::init();
