aba70846b1c2d4470f219cfcccc1830ae68f625f
Ivan Hennig <ivan@hennig.net.br>
2020-12-16 10:04:06 -0300

    Added new method for masking

--- src/builder/InputText.php
+++ src/builder/InputText.php
@@ -11,13 +11,29 @@ class InputText extends InputCommon
 {
     /**
      * Constraint, tamanho aceito em tela
-     * @var int
+     * @var ?int
      */
     public $maxlength = null;
-    /**
-     * Constraint, tamanho aceito em tela
-     * @var int
-     */
+
+    /** @var ?string */
     public $mask = null;
 
+    /**
+     * https://github.com/RobinHerbots/Inputmask
+     * Mask to be applied on component.
+     * 9 : numeric
+     * a : alphabetical
+     * * : alphanumeric
+     *
+     * i.e
+     * 99/99/9999
+     * aaa-9999
+     *
+     * @param string|null $mask
+     */
+    public function setMask(?string $mask): InputText
+    {
+        $this->mask = $mask;
+        return $this;
+    }
 }
